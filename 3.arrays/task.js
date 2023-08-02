8d3dacc0e4d7d175670f08094d9a26f570013372
Burdakova Daria <d.saharova@inbox.ru>
2023-08-02 17:36:26 +0300

    two problems solved

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+	let result = false;
+	if (arr1.length == arr2.length) {
+		result = arr1.every((element, index) => {
+			return element === arr2[index];
+		});
+	}
+	return result;
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let genderFilter = users.filter(user => user.gender === gender);
+	let sumAge = genderFilter.map((user) => user.age)
+		.reduce((acc, userAge) => acc + userAge, 0);
+	if (isNaN(sumAge / genderFilter.length)) {
+		return 0;
+	}
+	return sumAge / genderFilter.length;
 }
